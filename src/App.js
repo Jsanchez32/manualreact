fc7ddc5cf49b50f83c1736465e5e33f7cb65194a
JuanAndresSanchezRey <juanandressanchezrey@gmail.com>
2023-07-26 09:26:30 -0500

    feat: :sparkles: Manual React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+/* import React from "react";
+import ReactDOM from "react-dom";
+
+const CampuslandsApp = ()=>{
+  return (
+    <div>
+      <h1>Bienvenidos a Campuslands</h1>
+      <p>Explora nuestros campus y descubre nuevas experiencias</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<CampuslandsApp/>,document.getElementById('root'))
+
+export default CampuslandsApp; */
+
+//1.3.2 Construir primera aplicación de React como componentes//
+
+//Componentes para la importacion//
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+//Componentes Header//
+const Header = ()=>{
+  return(
+    <header>
+      <h1>Bienvenidos a Campuslands</h1>
+      <nav>
+        <a href="#">Inicio</a>
+        <a href="#">Campus</a>
+        <a href="#">Contacto</a>
+      </nav>
+    </header>
+  );
+};
+
+//Componentes Content//
+const Content = ()=>{
+  return(
+    <div>
+      <p>Explora nuestro campus y descubre nuevas experiencias</p>
+    </div>
+  );
+};
+
+//Componentes raiz App//
+
+const App = ()=>{
+  return(
+    <div>
+      <Header/>
+      <button className="explore-button">Explorar</button>
+      <Content/>
     </div>
   );
 }
